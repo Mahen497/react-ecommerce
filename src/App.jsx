d3eb2e711ef5eccaf6c532cee9fbf0ca1ed99151
mahen497 <patilmahendra497@gmail.com>
2025-10-08 22:04:18 +0530

    Added Home/ Checkout page Desgin

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router'
+import './App.css'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
